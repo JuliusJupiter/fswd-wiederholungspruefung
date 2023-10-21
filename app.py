6dcb26c9eec6884f3215453b4f960db1810912c4
JuliusJupiter <s_jeskej21(at)hwr-berlin.de>
2023-10-21 18:11:46 +0200

    fixed something

--- app.py
+++ app.py
@@ -15,10 +15,11 @@ from db import db, Todo, List, User, insert_sample   # (1.)
 
 login_manager = LoginManager()
 bootstrap = Bootstrap5(app)
+login_manager.init_app(app)
 
 @login_manager.user_loader
 def load_user(user_id):
-    return db.User.get(user_id)
+    return User.query.get(int(user_id))
 
 @app.route("/logout")
 @login_required
@@ -49,12 +50,12 @@ def register():
     form = forms.RegisterForm()
     if request.method == 'POST':
         if form.validate():
-            name = form.nutzername.data
+            name = form.username.data
             if User.query.filter_by(username=name).first():
                 flash('Username already exists. Please choose another.', 'danger')
                 return redirect(url_for('register'))
             hashed_password = generate_password_hash(form.password.data)
-            user = db.User(username='name', password=hashed_password)    
+            user = User(username= name, password=hashed_password)    
             db.session.add(user)
             db.session.commit()
             login_user(user)
@@ -62,17 +63,18 @@ def register():
     else:
         return render_template('register.html', form = form)    
 
-@login_required
+
 @app.route('/todos/', methods=['GET', 'POST'])
+@login_required
 def todos():
     form = forms.CreateTodoForm()
     if request.method == 'GET':
-        user_id = current_user().id
-        todos = Todo.query.filter_by(user_id=user_id).all()
+        user_id = current_user.id
+        todos = Todo.query.filter_by(user_id=user_id)
         return render_template('todos.html', todos=todos, form=form)
     else:  # request.method == 'POST'
         if form.validate():
-            todo = Todo(description=form.description.data)  # !!
+            todo = Todo(description=form.description.data, user_id=current_user.id)  # !!
             db.session.add(todo)  # !!
             db.session.commit()  # !!
             flash('Todo has been created.', 'success')
@@ -80,8 +82,9 @@ def todos():
             flash('No todo creation: validation error.', 'warning')
         return redirect(url_for('todos'))
 
-@login_required
+
 @app.route('/todos/<int:id>', methods=['GET', 'POST'])
+@login_required
 def todo(id):
     todo = db.session.get(Todo, id)  # !!
     form = forms.TodoForm(obj=todo)  # (2.)  # !!
@@ -113,14 +116,16 @@ def todo(id):
             flash('Nothing happened.', 'info')
             return redirect(url_for('todo', id=id))
 
-@login_required
+
 @app.route('/lists/')
+@login_required
 def lists():
     lists = db.session.execute(db.select(List).order_by(List.name)).scalars()  # (6.)  # !!
     return render_template('lists.html', lists=lists)
 
-@login_required
+
 @app.route('/lists/<int:id>')
+@login_required
 def list(id):
     list = db.session.get(List, id)  # !!
     if list is not None:
